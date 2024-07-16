cccfc062a6d9dcb0db71fd085105ce04fea35c18
Mamontosik <mamontosik@gmail.com>
2024-07-16 17:44:00 +0300

    add clean random

--- random_question.py
+++ random_question.py
@@ -1,22 +1,33 @@
 import random
 
-def get_random_question(filename):
+def get_random_question(filename, asked_questions):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             questions = file.readlines()
-            if questions:
-                return random.choice(questions).strip()
+            available_questions = [q.strip() for q in questions if q.strip() not in asked_questions]
+            if available_questions:
+                question = random.choice(available_questions)
+                asked_questions.add(question)
+                return question
             else:
-                return "Файл с вопросами пуст."
+                return None
     except FileNotFoundError:
         return "Файл не найден."
 
 if __name__ == "__main__":
     filename = '[file_address]/questions.txt'
+    asked_questions = set()
     
     while True:
-        random_question = get_random_question(filename)
-        print(random_question)
+        random_question = get_random_question(filename, asked_questions)
+        if random_question is None:
+            print("Все вопросы были заданы. Выход из программы.")
+            break
+        elif random_question == "Файл не найден.":
+            print(random_question)
+            break
+        else:
+            print(random_question)
         
         user_input = input("Хотите получить еще один вопрос? (да/нет): ").strip().lower()
         if user_input != 'да':
